c42762e04725f7e1cdd8a715c62eddef150bb4db
Anthony Hayward <anthonyhayward@me.comgit config --global user.email anthonyhayward@me.comgit config --global user.email anthonyhayward@me.com>
2020-12-01 14:47:23 -0800

    added eslint, git ignore, rough site created on index

---
+++ js/app.js
@@ -0,0 +1,28 @@
+'use strict'
+
+var userName = prompt('what is your name')
+console.log (userName)
+// if(userName = hi){document.write('userName');}
+// else {alert('try again, I said hi, you should say hi back')
+// }
+
+
+
+
+// if (userName = hi 
+
+var howOld = prompt('How old are you?');
+// prompt(howOld);
+console.log (howOld);
+
+
+var birthday = prompt('when is your birthday');
+// prompt(birthday);
+console.log(birthday);
+
+var color = prompt ('whats your favorite color');
+// prompt(color);
+console.log(color)
+
+alert(userName + ', ' + 'its nice to meet you! ' + howOld + ' ' + 'can be a tough age. ' + 'Wishing you a happy birthday on your special day, ' + birthday + '! ' + color + '!' + ', thats my fav color too!'  )
+
