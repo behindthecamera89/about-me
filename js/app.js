d29e42d5b2f2d8351fd26096b1d73f47222ad496
Anthony Hayward <anthonyhayward@me.comgit config --global user.email anthonyhayward@me.comgit config --global user.email anthonyhayward@me.com>
2020-12-01 16:51:39 -0800

    added 5 quetions prompts to site

--- js/app.js
+++ js/app.js
@@ -1,28 +1,58 @@
-'use strict'
+'use strict';
 
-var userName = prompt('what is your name')
-console.log (userName)
-// if(userName = hi){document.write('userName');}
-// else {alert('try again, I said hi, you should say hi back')
-// }
+var userName = prompt('What is your Name?');
+alert(userName + ', Welcome to my site!');
+// console.log('the username is ' + userName);
 
+// Im asking 5  y/n or yes or no question - responses can be entered lower case or upper case
+// Question 1
+var nameStartLetter = prompt('Does your name start with the letter A?').toUpperCase().toLowerCase();
+// // logging result to see that it works
+// // console.log (nameStartLetter);
+if (nameStartLetter === 'yes' || nameStartLetter === 'y') {
+  //   // console.log(nameStartLetter + ', ' + 'your name starts with the letter A, just like mine!);
+  alert(nameStartLetter + ', ' + 'your name starts with the letter A, just like mine!');
+}
+else {
+  alert('Wrong Anwser');
+}
 
+// Question 2
+var older30 = prompt('Are you older then 30?').toUpperCase().toLowerCase();
+if (older30 === 'yes' || older30 === 'y') {
+  //   // console.log('Congrats!' + userName + 'I\'m older then 30 too!');
+  alert('Congrats!' + userName + 'I\'m older then 30 too!');
+}
 
+// Question 3
+var outOfCountry = prompt('Have you been out of the Country?').toUpperCase().toLowerCase();
+if (outOfCountry === 'yes' || outOfCountry === 'y') {
+  //   // console.log(userName +', ' + 'we have something in common!');
+  alert(userName + ', ' + 'we have something in common!');
+}
+else {
+  alert('we need to fix that when covid is over!');
+}
 
-// if (userName = hi 
+// Question 4
+var ownCat = prompt('Do you own a Cat?').toUpperCase().toLowerCase();
+if (ownCat === 'yes' || ownCat === 'y') {
+  //   //   // console.log(userName + ', im so glad we have that in common, my cats name is Oakley');
+  alert(userName + ', im so glad we have that in common, my cats name is Oakley');
+}
+else {
+  alert(userName + ', you should get one!');
+}
 
-var howOld = prompt('How old are you?');
-// prompt(howOld);
-console.log (howOld);
+// Question 5
+var likePhoto = prompt('Do you like to take photos?').toUpperCase().toLowerCase();
+// console.log(likePhoto + ', seriously! That\'s a shame!');
+if (likePhoto === 'no' || likePhoto === 'n') {
+  alert(likePhoto + ', seriously! That\'s a shame!');
+}
+else {
+  alert('we should shoot some time!');
+}
 
-
-var birthday = prompt('when is your birthday');
-// prompt(birthday);
-console.log(birthday);
-
-var color = prompt ('whats your favorite color');
-// prompt(color);
-console.log(color)
-
-alert(userName + ', ' + 'its nice to meet you! ' + howOld + ' ' + 'can be a tough age. ' + 'Wishing you a happy birthday on your special day, ' + birthday + '! ' + color + '!' + ', thats my fav color too!'  )
+alert(userName + ', thank you for visiting my site! A lot of work needs to be done still!');
 
