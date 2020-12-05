bd6940f99aa5552ff19e82363f5c049d2983d19d
Anthony Hayward <anthonyhayward@me.comgit config --global user.email anthonyhayward@me.comgit config --global user.email anthonyhayward@me.com>
2020-12-04 18:39:38 -0800

    cleaned up code,added second javascript file with old code for later review if needed

--- js/Code_with_comments_for_review.js
+++ js/Code_with_comments_for_review.js
@@ -1,13 +1,21 @@
 'use strict';
 
+'use strict';
+
 var userName = prompt('What is your Name?');
 alert(userName + ', Welcome to my site!');
+// console.log('the username is ' + userName);
 var anwserdCorrectly = 0;
 
+// Im asking 5  y/n or yes or no question - responses can be entered lower case or upper case
 function questionOne() {
+  // Question 1
   var nameStartLetter = prompt('Does your name start with the letter A?').toLowerCase();
+  // // logging result to see that it works
+  // // console.log (nameStartLetter);
   if (nameStartLetter === 'yes' || nameStartLetter === 'y') {
     anwserdCorrectly++;
+    //   // console.log(nameStartLetter + ', ' + 'your name starts with the letter A, just like mine!);
     alert(nameStartLetter + ', ' + 'your name starts with the letter A, just like mine!');
   }
   else {
@@ -17,18 +25,22 @@ function questionOne() {
 questionOne();
 
 function questionTwo() {
+  // Question 2
   var older30 = prompt('Are you older then 30?').toLowerCase();
   if (older30 === 'yes' || older30 === 'y') {
     anwserdCorrectly++;
+    //   // console.log('Congrats!' + userName + 'I\'m older then 30 too!');
     alert('Congrats! ' + userName + ' I\'m older then 30 too!');
   }
 }
 questionTwo();
 
 function questionThree() {
+  // Question 3
   var outOfCountry = prompt('Have you been out of the Country?').toLowerCase();
   if (outOfCountry === 'yes' || outOfCountry === 'y') {
     anwserdCorrectly++;
+    //   // console.log(userName +', ' + 'we have something in common!');
     alert(userName + ', ' + 'we have something in common!');
   }
   else {
@@ -38,9 +50,11 @@ function questionThree() {
 questionThree();
 
 function questionFour() {
+  // Question 4
   var ownCat = prompt('Do you own a Cat?').toLowerCase();
   if (ownCat === 'yes' || ownCat === 'y') {
     anwserdCorrectly++;
+    //   //   // console.log(userName + ', im so glad we have that in common, my cats name is Oakley');
     alert(userName + ', im so glad we have that in common, my cats name is Oakley');
   }
   else {
@@ -50,7 +64,9 @@ function questionFour() {
 questionFour();
 
 function questionFive() {
+  // Question 5
   var likePhoto = prompt('Do you like to take photos?').toLowerCase();
+  // console.log(likePhoto + ', seriously! That\'s a shame!');
   if (likePhoto === 'no' || likePhoto === 'n') {
     anwserdCorrectly++;
     alert(likePhoto + ', seriously! That\'s a shame!');
@@ -64,6 +80,9 @@ questionFive();
 alert(userName + ', thank you for visiting my site! A lot of work needs to be done still!');
 
 function questionSix() {
+  // Question 6
+  // Create a Guessing game with 4 numbers
+
   var correctNumber = 3;
   var guessCorrectly = false;
   alert('Lets play a guessing game!');
@@ -89,14 +108,21 @@ function questionSix() {
 questionSix();
 
 function questionSeven() {
+  // Question 7
+  // Create a guessing game with multiple anwsers
+
   var correctNumbers = [2, 4, 6, 8, 10];
+  // var guessCorrectly2 = false;
   alert('Lets play a guessing game that has multiple right anwsers!');
   loop1: for (var c = 0; c < 6; c++) {
     var guess2 = parseInt(prompt(`${userName}, Please pick a number between 1 and 20. There are multiple right anwsers`));
+    // console.log(correctNumbers[c]);
+    // console.log(guess2, 'guess 2');
     for (var j = 0; j < correctNumbers.length; j++) {
       if (guess2 === correctNumbers[j]) {
         anwserdCorrectly++;
         alert('You guessed correctly');
+        // guessCorrectly2 = true;
         break loop1;
       }
       else if (guess2 !== correctNumbers[c]) {
@@ -104,7 +130,11 @@ function questionSeven() {
         break;
       }
     }
+    // else if (guessCorrectly2) {
+    //   alert('You chose a correct number!');
+    // }
   }
+  // alert(`The correct anwsers are ${correctNumbers}`);
 }
 questionSeven();
 
